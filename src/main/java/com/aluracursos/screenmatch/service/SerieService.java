d9f38b055056240ea8bd58468f958e0a4b4759cc
JonathanAPD <japd182@gmail.com>
2024-05-22 00:53:03 -0500

    creando servicio para top 5 series y modificando controlador para mapear el End Point series/top5

--- src/main/java/com/aluracursos/screenmatch/service/SerieService.java
+++ src/main/java/com/aluracursos/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package com.aluracursos.screenmatch.service;
 
 import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.model.Serie;
 import com.aluracursos.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,15 @@ public class SerieService {
     private SerieRepository repository;
 
     public List<SerieDTO> obtenerTodasLasSeries(){
-        return repository.findAll().stream()
+        return convieteDatos(repository.findAll());
+    }
+
+    public List<SerieDTO> obtenerTop5() {
+        return convieteDatos(repository.findTop5ByOrderByEvaluacionDesc());
+    }
+
+    public List<SerieDTO> convieteDatos(List<Serie> serie){
+        return serie.stream()
                 .map(s -> new SerieDTO(s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(), s.getPoster(),
                         s.getGenero(),s.getActores(),s.getSinopsis()))
                 .collect(Collectors.toList());
